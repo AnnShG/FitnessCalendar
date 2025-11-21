2bf6d5a9658256daf574dc37ef9d92b1a53a3fe5
sharu <sharu@DESKTOP-4CD36IM>
2025-11-21 10:43:14 +0200

    Changed AppDB

--- app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
+++ app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
@@ -12,19 +12,19 @@ import com.example.fitnesscalendar.entities.User;
 @Database(entities= {User.class}, version = 1) // array list with users
 public abstract class AppDatabase extends RoomDatabase {
 
+    // create one database only once - INSTANCE
     private static volatile AppDatabase INSTANCE; // singleton instance
 
     public abstract UserDao userDao();
 
     public static AppDatabase getDatabase(Context context) {
-        if (INSTANCE == null) {
-            synchronized (AppDatabase.class) {
-                if (INSTANCE == null) {
+        if (INSTANCE == null) { // not synced
+            synchronized (AppDatabase.class) { // one thread at a time
+                if (INSTANCE == null) { // second check if it is synced
                     INSTANCE = Room.databaseBuilder(
                             context.getApplicationContext(),
                             AppDatabase.class,
-                            "fitnes" +
-                                    "s_calendar.db"
+                            "fitness_calendar.db"
                     ).fallbackToDestructiveMigration().build();
                 }
             }
