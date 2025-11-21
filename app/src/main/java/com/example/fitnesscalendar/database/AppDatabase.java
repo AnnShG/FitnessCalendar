d34f1041048156bbac849b3ec824fef4925634f2
sharu <sharu@DESKTOP-4CD36IM>
2025-11-21 10:25:14 +0200

    Changed AppDB

--- app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
+++ app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
@@ -1,11 +1,35 @@
 package com.example.fitnesscalendar.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.fitnesscalendar.dao.UserDao;
 import com.example.fitnesscalendar.entities.User;
 
-@Database(entities= {User.class}, version = 1)
+@Database(entities= {User.class}, version = 1) // array list with users
 public abstract class AppDatabase extends RoomDatabase {
+
+    private static volatile AppDatabase INSTANCE; // singleton instance
+
+    public abstract UserDao userDao();
+
+    public static AppDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "fitnes" +
+                                    "s_calendar.db"
+                    ).fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
 }
