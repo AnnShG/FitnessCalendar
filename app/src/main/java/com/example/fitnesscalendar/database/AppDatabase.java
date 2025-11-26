1df4073ce0da4765db064f743095c394bb287038
Mardoka Mardoka <mardokamardoka7@gmail.com>
2025-11-26 21:28:03 +0200

    Refactored database and added Lombok
    Added UserDao with CRUD operations and queries.
    Integrated Lombok for the User entity.
    Updated AppDatabase to include UserDao.
    Fixed checkable attribute in survey_page_2.xml.

--- app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
+++ app/src/main/java/com/example/fitnesscalendar/database/AppDatabase.java
@@ -3,9 +3,12 @@ package com.example.fitnesscalendar.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.fitnesscalendar.dao.UserDao;
 import com.example.fitnesscalendar.entities.User;
 
 @Database(entities= {User.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract UserDao userDao();
 
 }
--- app/src/main/java/com/example/fitnesscalendar/entities/User.java
+++ app/src/main/java/com/example/fitnesscalendar/entities/User.java
@@ -1,7 +1,13 @@
 package com.example.fitnesscalendar.entities;
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+import lombok.Data;
+
+@Data
 @Entity(tableName = "users")
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
 }
