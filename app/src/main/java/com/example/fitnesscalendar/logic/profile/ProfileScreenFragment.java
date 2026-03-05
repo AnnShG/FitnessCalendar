c574687c781386ac9f85c8a79fb7acdee0e1eac8
sharu <sharu@DESKTOP-4CD36IM>
2026-03-05 16:43:06 +0200

    Implemented an adapter that creates frames for adding the data to the screen

--- app/src/main/java/com/example/fitnesscalendar/logic/profile/ProfileScreenFragment.java
+++ app/src/main/java/com/example/fitnesscalendar/logic/profile/ProfileScreenFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.fitnesscalendar.R;
 import com.example.fitnesscalendar.databinding.ProfileScreenBinding;
@@ -16,13 +17,13 @@ import lombok.NonNull;
 public class ProfileScreenFragment extends Fragment {
 
     private ProfileScreenBinding binding;
+    private GoalAdapter goalAdapter;
 
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = ProfileScreenBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -30,6 +31,12 @@ public class ProfileScreenFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        goalAdapter = new GoalAdapter();
+        // Setup the RecyclerView
+        // they run as soon as the screen is ready
+        binding.goalsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.goalsRecyclerView.setAdapter(goalAdapter);
     }
 
 
