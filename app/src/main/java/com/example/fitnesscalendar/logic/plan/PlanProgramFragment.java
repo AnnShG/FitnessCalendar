b852ba7d44ce947781f11e6826fb09ece4f4bbba
AnnShG <anna.shkajeva@gmail.com>
2026-03-29 17:20:21 +0300

    Plan program fragment prepared for representing the screen UI

--- app/src/main/java/com/example/fitnesscalendar/logic/plan/PlanProgramFragment.java
+++ app/src/main/java/com/example/fitnesscalendar/logic/plan/PlanProgramFragment.java
@@ -1,6 +1,39 @@
 package com.example.fitnesscalendar.logic.plan;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.fitnesscalendar.databinding.AddExerciseScreenBinding;
+import com.example.fitnesscalendar.databinding.PlanProgramScreenBinding;
+
+import lombok.NonNull;
+
 public class PlanProgramFragment extends Fragment {
+
+    private PlanProgramScreenBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = PlanProgramScreenBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
