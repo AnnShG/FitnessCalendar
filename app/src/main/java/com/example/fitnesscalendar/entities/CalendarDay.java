d6416bb3927c768d815347cf93a225cdd567f069
AnnShG <anna.shkajeva@gmail.com>
2026-03-29 17:22:11 +0300

    Added calendar day entity foreign key

--- app/src/main/java/com/example/fitnesscalendar/entities/CalendarDay.java
+++ app/src/main/java/com/example/fitnesscalendar/entities/CalendarDay.java
@@ -2,6 +2,7 @@ package com.example.fitnesscalendar.entities;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 import java.util.Date;
@@ -9,10 +10,17 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-@Entity(tableName = "calendar_days")
+@Entity(tableName = "calendar_days",
+        foreignKeys = @ForeignKey(entity = User.class,
+                parentColumns = "id",
+                childColumns = "user_id",
+                onDelete = ForeignKey.CASCADE))
 public class CalendarDay {
     @PrimaryKey(autoGenerate = true)
     public long id;
+
+    @ColumnInfo(name = "user_id")
+    public long userId;
 
     @ColumnInfo(name = "date")
     public Date date;
