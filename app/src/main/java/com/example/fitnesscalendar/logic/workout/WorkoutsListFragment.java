d113397e7e48b9cfdec8aec5dfb59e27b4b03bf9
sharu <sharu@DESKTOP-4CD36IM>
2026-03-31 15:29:56 +0300

    Fixed the card wideness - wider

--- app/src/main/java/com/example/fitnesscalendar/logic/workout/WorkoutsListFragment.java
+++ app/src/main/java/com/example/fitnesscalendar/logic/workout/WorkoutsListFragment.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fitnesscalendar.R;
@@ -47,7 +47,7 @@ public class WorkoutsListFragment extends Fragment {
 
         RecyclerView recyclerView = view.findViewById(R.id.workoutsRecyclerView);
         if (recyclerView != null) {
-            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
             recyclerView.setAdapter(workoutAdapter);
             recyclerView.setNestedScrollingEnabled(false);
         }
