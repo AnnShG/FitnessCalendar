1d5cc33f4820dc5d58ce5801c08cfebf90c4ab53
sharu <sharu@DESKTOP-4CD36IM>
2026-03-31 14:35:54 +0300

    Adapter and calendar manager for initialising the calendar view and listen to the buttons

--- app/src/main/java/com/example/fitnesscalendar/logic/plan/PlanProgramFragment.java
+++ app/src/main/java/com/example/fitnesscalendar/logic/plan/PlanProgramFragment.java
@@ -7,14 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-import com.example.fitnesscalendar.databinding.AddExerciseScreenBinding;
 import com.example.fitnesscalendar.databinding.PlanProgramScreenBinding;
+import com.example.fitnesscalendar.logic.calendar.CalendarAdapter;
+import com.example.fitnesscalendar.logic.calendar.CalendarManager;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import lombok.NonNull;
 
 public class PlanProgramFragment extends Fragment {
 
     private PlanProgramScreenBinding binding;
+    private CalendarAdapter adapter;
+
+    private CalendarManager manager = new CalendarManager();
+    private Set<String> highlightedDates = new HashSet<>();
+    private final List<String> daysList = new ArrayList<>(); // takes a list of numbers/dates 1,2,3,4
+
 
     @Override
     public View onCreateView(
@@ -29,6 +41,50 @@ public class PlanProgramFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        //  listener for date selection
+        adapter = new CalendarAdapter(daysList, (position, dayText) -> {
+            if (!dayText.isEmpty()) {
+                // get the unique key for the date
+                String dateKey = manager.getDateKeyForDay(dayText);
+
+                if (highlightedDates.contains(dateKey)) {
+                    highlightedDates.remove(dateKey);
+                } else {
+                    highlightedDates.add(dateKey);
+                }
+
+                adapter.setHighlightedDates(highlightedDates, manager);
+
+                if (!highlightedDates.isEmpty()) {
+                    binding.btnApply.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        binding.calendarRecyclerView.setAdapter(adapter);
+
+        updateUI();
+
+        binding.calendarPrevButton.setOnClickListener(v -> {
+            manager.goToPrevMonth();
+            updateUI();
+        });
+
+        binding.calendarNextButton.setOnClickListener(v -> {
+            manager.goToNextMonth();
+            updateUI();
+        });
+//
+//        Calendar cal = manager.getCurrentCalendar();
+//        cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dayText));
+//        Date selectedDate = cal.getTime(); // Now you have the Date to save in DB!
+
+    }
+
+    private void updateUI() {
+        binding.monthAndYear.setText(manager.getHeaderString());
+        List<String> days = manager.getDaysOfMonthList();
+        adapter.setDays(days);
     }
 
     @Override
