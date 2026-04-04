027f11d6798587e5d154236ad5c6012440cea729
sharu <sharu@DESKTOP-4CD36IM>
2026-04-04 19:38:49 +0300

    POJO class is created to gather specific data from the workouts table

---
+++ app/src/main/java/com/example/fitnesscalendar/relations/PlannedWorkoutInfo.java
@@ -0,0 +1,14 @@
+package com.example.fitnesscalendar.relations;
+
+import androidx.room.ColumnInfo;
+
+/**
+ * A Projection POJO class used to represent a summary of a workout that has been scheduled on the calendar.
+ * It is used by the DAO to return a specific subset of columns (ID, Title, Colour) from the 'workouts' table
+ */
+public class PlannedWorkoutInfo {
+    @ColumnInfo(name = "workout_id")
+    public long workout_id;
+    public String title;
+    public Integer colour;
+}
